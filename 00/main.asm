34a2c3c9ac13ed665a5c884e2e289c15ff813a09
juusokasperi <juusor@gmail.com>
2025-12-02 05:29:15 +0000

    fix for buffere ending mid-line

--- 00/main.asm
+++ 00/main.asm
@@ -3,9 +3,10 @@
 %define SYS_OPEN 	2
 %define SYS_CLOSE 	3
 %define SYS_EXIT 	60
+%define BUFFER_SIZE 65536
 
 section .bss
-	buffer: resb 65536	; for read
+	buffer: resb BUFFER_SIZE	; for read
 	num_buffer: resb 64 	; for number to string conversion
 
 section .text
@@ -19,39 +20,76 @@ _start:
 	pop rsi 	; argv[1]
 
 .initialize:
-	mov rbp, 50	; dial position (starts at 50)
-	mov r12, 0	; zero counter
+	mov rbp, 50		; dial position (starts at 50)
+	xor r12, r12	; zero counter
+	xor r13, r13	; total bytes in buffer 
+	xor r14, r14 	; current buffer index 
 
 .open:
 	; open(path, flag, mode)
 	mov rax, SYS_OPEN
 	mov rdi, rsi		; filename to rdi
-	mov rsi, 0			; 0 = O_RDONLY
+	xor rsi, rsi		; 0 = O_RDONLY
 	syscall				; returns fd in rax
 	cmp rax, -1
 	je .exit_err
 	mov rbx, rax		; save the fd to rbx
 
-.read_buffer:
-	; read(fd, buffer, count)
-	mov rax, SYS_READ
-	mov rdi, rbx 
-	mov rsi, buffer 
-	mov rdx, 65536
-	syscall
+	lea r8, [.process]
+	jmp .refill_buffer
 
-	cmp rax, 0			; 0 == eof
-	je .done_reading
-	cmp rax, -1			; -1 == error
-	je .err_close_fd
+.refill_buffer:
+	push rcx			; Push rcx and r8 to stack for duration of syscall  
+	push r8 
 
-	mov r13, rax		; save amount of bytes read to r13
-	xor r14, r14		; use r14 as index, set it to 0
+	sub r13, r14 		; total bytes in buffer - current index 
+	mov r10, r13 
+
+	cmp r10, 0 
+	je .read_new_chunk
+
+	mov rcx, r10 
+	lea rsi, [buffer + r14]
+	mov rdi, buffer 
+	rep movsb			; copy r10 (remaining bytes) from rsi (buffer + index) to rdi (buffer)
+
+.read_new_chunk:
+	xor r14, r14
+
+	mov rax, SYS_READ 
+	mov rdi, rbx
+	lea rsi, [buffer + r13]	; append to potential partial data
+	mov rdx, BUFFER_SIZE
+	sub rdx, r10 			; read BUFFER_SIZE - remaining bytes from last read 
+	syscall 
+
+	cmp rax, 0 
+	je .pop_and_exit_eof
+	cmp rax, -1
+	je .pop_and_exit_err
+	add r13, rax 
+	pop r8					; where to jmp to 
+	pop rcx 				; restore RCX (our accumulator)
+	jmp r8
+
+.pop_and_exit_eof:
+	pop r8
+	pop rcx 
+	jmp .done_reading 
+
+.pop_and_exit_err:
+	pop r8 
+	pop rcx 
+	jmp .err_close_fd
 
 .process:
-	cmp r14, r13		; if we processed all bytes, read more
-	jge .read_buffer
+	cmp r14, r13		; if we still have chars to read 
+	jl .scan_char
 
+	lea r8, [.process]
+	jmp .refill_buffer
+
+.scan_char:
 	movzx rax, byte [buffer + r14]	; load one byte to rax
 
 	cmp rax, 0x4c 		; if byte is 'L'
@@ -63,6 +101,16 @@ _start:
 
 	jmp .err_close_fd
 
+.sign_negate:
+	mov r15, -1
+	inc r14				; move past the sign (L) 
+	jmp .parse_number
+
+.sign:
+	mov r15, 1
+	inc r14				; move past the sign (R)
+	jmp .parse_number	
+
 .skip_newline:
 	inc r14 
 	jmp .process
@@ -71,6 +119,13 @@ _start:
 	xor rcx, rcx		; use this to count the number
 
 .digit_loop:
+	cmp r14, r13
+	jl .read_digit
+
+	lea r8, [.digit_loop]
+	jmp .refill_buffer 
+
+.read_digit:
 	movzx rax, byte [buffer + r14]
 	cmp rax, 0x0a 		; '\n'
 	je .apply_rotation	; if newline, done with reading the number
@@ -114,16 +169,6 @@ _start:
 
 .loop_done:
 	jmp .process 
-
-.sign_negate:
-	mov r15, -1
-	inc r14				; move past the sign (L) 
-	jmp .parse_number
-
-.sign:
-	mov r15, 1
-	inc r14				; move past the sign (R)
-	jmp .parse_number	
 
 .done_reading:
 	mov rax, SYS_CLOSE 
