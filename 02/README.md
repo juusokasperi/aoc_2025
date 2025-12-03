2b5439fe66f91b32ef2bbb41146e8870cc2daa9e
juusokasperi <juusor@gmail.com>
2025-12-03 20:25:31 +0000

    64 bit SIMD, updated readme

--- 02/README.md
+++ 02/README.md
@@ -4,8 +4,11 @@ Written in Assembly for Linux x86_64.
 
 For part2, I tried my hand at some optimization;
 
-- SIMD intrinsics for faster reading of the buffer.
-- Removed read/close syscalls - program only reads stdin
+- SIMD intrinsics for faster reading of the buffer. `part2_optimized_simd` attempts to read 32 bytes at once, `part2_optimized_simd_64` 64 bytes.
+- Removed read/close syscalls for the `_optimized_` binaries - programs only read stdin (`./part2_optimized_simd_64 < input`)
+- Plus, added -static flag to linker since the program works standalone
+
+With this I achieve very fast results, with the input given by AoC, timing gets my whole program to run ~1.4ms.
 
 ### How to build:
 
@@ -13,6 +16,7 @@ There are a couple binaries you can build, namely;
 ```sh
 part1           part2
 part2_simd      part2_optimized_simd
+part2_optimized_simd_64
 ```
 
 `binary=(binary_to_build); nasm -f elf64 ${binary}.asm -o ${binary}.o && ld ${binary}.o -o ${binary} -static`
---
+++ 02/part2_optimized_simd_64.asm
@@ -0,0 +1,248 @@
+%include "os_defs.inc"
+
+%define SYS_WRITE	1
+%define SYS_READ	0
+%define SYS_OPEN 	2
+%define SYS_CLOSE 	3
+%define SYS_FSTAT 	5
+%define SYS_LSEEK	8
+%define SYS_MMAP	9
+%define SYS_EXIT 	60
+%define STDOUT 		1
+%define STDIN 		0
+
+%define SEEK_END 	2
+%define BUFFER_SIZE 2097152 ; 2MiB
+
+default rel
+align 16
+
+section .data
+
+section .bss
+	buffer  	resb BUFFER_SIZE
+	num_buffer	resb 64
+
+section .text
+	global _start
+
+_start:
+	mov rax, SYS_READ 
+	mov rdi, STDIN
+	lea rsi, [buffer]
+	mov rdx, BUFFER_SIZE
+	syscall
+
+	test rax, rax 
+	jle .exit_error
+
+	; rsi: Current character pointer 
+	; r14: End of buffer pointer 
+	; r12: Total joltage sum
+	mov r14, rax 				; total bytes read 
+	lea rsi, [buffer] 			; rsi = start 
+	add r14, rsi 				; r14 = end 
+	xor r12, r12 				; sum = 0
+
+.line_loop:
+	cmp rsi, r14				; if at end 
+	jge .done_reading
+
+	; for part 2 we need to first find newline to calculate bounds
+	mov r8, rsi 
+
+.find_newline:
+	cmp r8, r14					; check eof 
+	jge .found_line_end 
+	cmp byte [r8], 0x0a 		; check nl 
+	je .found_line_end 
+	inc r8 
+	jmp .find_newline 
+
+.found_line_end:
+	; rsi = start of curr line 
+	; r8 = address of nl / eof for this line 
+	xor r13, r13 
+	mov rcx, 12 				; looking for 12 digits 
+	mov r9, rsi 				; search start pointer 
+
+.digit_search_loop:
+	mov rbx, r8 				; rbx = nl address 
+	sub rbx, rcx 				; rbx = last valid index (nl - 12)
+
+	xor rdx, rdx 				; rdx = best digit found so far 
+	mov r10, r9 				; r10 = iterator
+	mov r11, r9					; r11 = addr of best digit found 
+
+	mov al, byte [r10]
+	sub al, '0'
+	movzx rdx, al
+	inc r10 
+; --- 64 byte simd ---
+.scan_candidates:
+	mov rax, r10 
+	add rax, 63
+	cmp rax, rbx
+	jg .try_simd_32
+	cmp rax, r14 
+	jg .try_simd_32 
+
+	vmovdqu ymm0, [r10]			; chunk a (bytes 0-31)
+	vmovdqu ymm3, [r10 + 32]	; chunk b (byres 32-63)
+
+	movzx eax, dl 
+	add eax, '0'
+	vmovd xmm1, eax 
+	vpbroadcastb ymm1, xmm1
+
+	vpcmpgtb ymm2, ymm0, ymm1 	; mask A 
+	vpcmpgtb ymm4, ymm3, ymm1 	; mask B 
+
+	; combine result 
+	vpor ymm5, ymm2, ymm4 
+	vptest ymm5, ymm5 
+	jz .skip_64_bytes
+
+	; if match found, check which chunk 
+	vpmovmskb eax, ymm2 
+	test eax, eax 
+	jnz .found_in_chunk_a
+
+	vpmovmskb eax, ymm4 
+	tzcnt eax, eax 
+	add r10, 32 
+	jmp .handle_match_offset 
+
+.found_in_chunk_a:
+	tzcnt eax, eax 
+
+.handle_match_offset: 
+	add r10, rax 
+	mov al, byte [r10]
+	sub al, '0'
+	movzx rdx, al 
+	mov r11, r10 
+
+	inc r10 
+	jmp .scan_candidates 
+
+.skip_64_bytes:
+	add r10, 64 
+	jmp .scan_candidates 
+
+; -- first fallback (32 byte simd) --
+.try_simd_32:
+	mov rax, r10 
+	add rax, 31
+	cmp rax, rbx 
+	jg .scalar_fallback 
+	cmp rax, r14 
+	jg .scalar_fallback 
+
+	movzx eax, dl 
+	add eax, '0'
+	vmovd xmm1, eax
+	vpbroadcastb ymm1, xmm1 
+	
+	vmovdqu ymm0, [r10]
+	vpcmpgtb ymm2, ymm0, ymm1 
+
+	vpmovmskb eax, ymm2 
+
+	test eax, eax 
+	jz .skip_32_bytes
+
+	tzcnt eax, eax 
+	add r10, rax 
+
+	mov al, byte [r10]
+	sub al, '0'
+	movzx rdx, al
+	mov r11, r10
+
+	inc r10
+	jmp .scan_candidates
+
+.skip_32_bytes:
+	add r10, 32 
+	jmp .scan_candidates
+
+.scalar_fallback:
+	cmp r10, rbx 				; while r10 <= rbx 
+	jg .found_max_digit 		; if i > limit, done 
+
+	mov al, byte [r10]
+	sub al, '0'
+	cmp al, dl 					; if new digit <= oldmax, don't upd 
+	jle .skip_update 
+	movzx rdx, al				; upd max value 
+	mov r11, r10 				; upd max addr 
+
+.skip_update:
+	inc r10 
+	jmp .scalar_fallback 
+
+.found_max_digit:
+	imul r13, r13, 10 
+	add r13, rdx
+
+	mov r9, r11 				; upd search pointer 
+	inc r9
+
+	dec rcx 					; decrement digits needed ctr 
+	jnz .digit_search_loop 
+
+	add r12, r13 				; upd to total 
+
+	lea rsi, [r8 + 1]
+	jmp .line_loop 
+
+.done_reading:
+	; r12 = total sum 
+	mov rax, r12
+	lea rsi, [num_buffer + 63]	; build from end to start
+	mov byte [rsi], 0x0a 
+	dec rsi 
+
+	cmp rax, 0 
+	jne .convert_loop 
+	mov byte [rsi], 0x30 
+	dec rsi 
+	jmp .print_result 
+
+.convert_loop:
+	cmp rax, 0 
+	je .print_result 
+
+	xor rdx, rdx 
+	mov rcx, 0x0a 
+	div rcx 
+	add dl, 0x30 
+	mov [rsi], dl 
+	dec rsi 
+	jmp .convert_loop 
+
+.print_result:
+	inc rsi 
+
+	lea rdx, [num_buffer + 64]
+	sub rdx, rsi 
+	mov rax, SYS_WRITE 
+	mov rdi, STDOUT
+	syscall 
+
+.exit_success:
+	mov rax, SYS_EXIT 
+	xor rdi, rdi 
+	syscall 
+
+.exit_close_fd:
+	mov rax, SYS_CLOSE 
+	mov rdi, r15
+	syscall 
+
+.exit_error:
+	mov rax, SYS_EXIT 
+	mov rdi, 1 
+	syscall 
+
